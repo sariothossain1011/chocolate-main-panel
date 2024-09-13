d7c48d06e1e50294818793b83758e11c7e85fbd8
sariothossain1011 <sariothossain1011@gmail.com>
2024-09-14 03:14:28 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=' text-center'>
+      <h1 className=' text-xl  font-semibold'>I'm Chocolate Home Page...</h1>
+    </div>
+  )
+}
+
+export default Home

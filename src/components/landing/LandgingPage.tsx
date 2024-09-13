dc4caeb352e87a4fb852b46e484fbc078e883a82
sariothossain1011 <sariothossain1011@gmail.com>
2024-09-14 03:25:36 +0600

    vercel error solved

--- src/components/landing/LandgingPage.tsx
+++ src/components/landing/LandgingPage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LandgingPage = () => {
   return (
     <div className=' text-center py-20'>
-      <h1 className=' text-4xl  font-bold '>I'm Chocolate Home Page...</h1>
+      <h1 className=' text-4xl  font-bold '>I&apos;m Chocolate Home Page...</h1>
       <p className='text-sm font-medium'>Creating by SARIOT HOSSAIN</p>
     </div>
   )

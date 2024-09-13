14475b0469df0ae65ceba41412f6de9409fe5baf
sariothossain1011 <sariothossain1011@gmail.com>
2024-09-14 03:21:47 +0600

    some folder created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
+import LandgingPage from '@/components/landing/LandgingPage'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className=' text-center'>
-      <h1 className=' text-xl  font-semibold'>I'm Chocolate Home Page...</h1>
-    </div>
+    <LandgingPage />
   )
 }
 
---
+++ src/components/landing/LandgingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandgingPage = () => {
+  return (
+    <div className=' text-center py-20'>
+      <h1 className=' text-4xl  font-bold '>I'm Chocolate Home Page...</h1>
+      <p className='text-sm font-medium'>Creating by SARIOT HOSSAIN</p>
+    </div>
+  )
+}
+
+export default LandgingPage
